05aa811794ad0a41bd08ab45142fdea165823bec
adm_pietrzyk1 <adm_pietrzyk1@nobian.com>
2026-07-08 20:17:37 +0200

    Use TFC logo for site icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "projectile-site-v13-i18n-language-fix-mobile-buttons-v2";
+const CACHE_NAME = "projectile-site-v13-i18n-language-fix-tfc-icons-v4";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
@@ -52,8 +52,20 @@ const STATIC_ASSETS = [
   "./offline.html",
   "./404.html",
   "./500.html",
-  "./manifest.json",
-  "./assets/projectile-mark.svg"
+  "./manifest.json?v=4",
+  "./favicon.ico?v=4",
+  "./apple-touch-icon.png?v=4",
+  "./assets/icons/favicon-16.png?v=4",
+  "./assets/icons/favicon-32.png?v=4",
+  "./assets/icons/favicon-48.png?v=4",
+  "./assets/icons/favicon-64.png?v=4",
+  "./assets/icons/icon-180.png",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png",
+  "./assets/icons/maskable-512.png",
+  "./assets/icons/site-icon-512.png",
+  "./assets/tfc-logo-simple.png",
+  "./assets/og-tfc.png"
 ];
 
 self.addEventListener("install", (event) => {
